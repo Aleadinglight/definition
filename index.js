e773e5587705d93569b1a679dcfcc1ad3e250700
aleadinglight <huutam.ptnk@gmail.com>
2018-07-15 05:30:53 +0700

    added more files

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-var unirest = require('unirest');
-var fs = require("fs");
-var key = fs.readFileSync("key.txt");
-
-unirest.get("https://wordsapiv1.p.mashape.com/words/bump/definitions")
-    .header("X-Mashape-Key", key)
-    .header("X-Mashape-Host", "wordsapiv1.p.mashape.com")
-    .end(function (result) {
-        if (result.status!=200){
-            console.log("Bad response!\n", result.status);
-        }
-        else{
-            console.log(result.body);
-        }
+var result = new Vue({
+    el:'#result',
+    data: {
+        message:""
     }
-);
+});
+
+
