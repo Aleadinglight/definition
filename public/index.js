dbf141daf0bbb5a6a5eb1c8eca4d0e5546be96b4
aleadinglight <huutam.ptnk@gmail.com>
2018-07-17 08:43:38 +0700

    added display message

--- public/index.js
+++ public/index.js
@@ -1,13 +1,16 @@
 var result = new Vue({
     el:'#result',
     data: {
-        message:""
+        message:"",
+        returnMessage:""
     },
     methods:{
         findDef: function(){
           $.get( "/search", { name: this.message })
             .done(function( data ){
               console.log(data);
+              console.log(JSON.stringify(data));
+              this.returnMessage = JSON.stringify(data);
           });
         }
     }
