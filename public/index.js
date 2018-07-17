0495574316efd69cfdd93ceafeaf6a742db1114b
aleadinglight <huutam.ptnk@gmail.com>
2018-07-17 15:15:21 +0700

    styling final and fixed exception

--- public/index.js
+++ public/index.js
@@ -11,15 +11,21 @@ var result = new Vue({
         findDef: function(){
             result.isDisabled = true;
             result.show=false;
-            $.get( "/search", { name: this.message })
+            $.get( "/search", { name: result.message })
                 .done(function( data ){
                 console.log(data);
-                result.returnWord=data.word.charAt(0).toUpperCase()+data.word.slice(1);
-                var m_returnMessage =" \n";
-                data.definitions.forEach(function(element,i){
-                    m_returnMessage+=(i+1)+". "+element.definition+"\n";
-                });
-                result.returnMessage = m_returnMessage;
+                if (data==="Bad response from server!"){
+                    result.returnWord="";
+                    result.returnMessage = "Sorry, we cannot found this word!";
+                }
+                else{
+                    result.returnWord=data.word.charAt(0).toUpperCase()+data.word.slice(1);
+                    var m_returnMessage =" \n";
+                    data.definitions.forEach(function(element,i){
+                        m_returnMessage+=(i+1)+". "+element.definition+"\n";
+                    });
+                    result.returnMessage = m_returnMessage;
+                }
                 result.isDisabled = false;
                 result.show=true;
             });
